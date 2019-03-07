e76ccf6b1667bb921a8623c99c17440765ec5bb1
hreidar17 <hreidar17@ru.is>
2019-03-07 22:56:00 +0000

    trying to implement a better bundles site

--- src/components/BundleItem/index.js
+++ src/components/BundleItem/index.js
@@ -7,18 +7,11 @@ import { findBubbleProduct } from '../../services/BubbleService'
 
 function getProducts(arr) {
     var products = [];
-
-    console.log('test')
-
+    //This function retrieves the items from the bundlelist
     for(var i = 0; i < arr.length; i++){
-      console.log('loop '+i+': '+arr[i])
       findBubbleProduct(arr[i]).then( item =>
-        (products.push(item.name))
+        (products.push({id: item.id, name: item.name, price: item}))
       );
-    }
-
-    for(var j = 0; j < products.length; j++){
-      console.log('after fetch: '+products[j])
     }
 
     return products;
@@ -33,7 +26,8 @@ const BundleItem = props => {
             <div className="card-header">{ name } <i className="fa fa-2x fa-times" style={{ float: 'right' }}></i></div>
             <div className="card-body">
                 <div className="card-text">
-                {items.map(pl => <Link to={ "/products/" + pl }>Item {<br />}</Link>)}
+
+                {items.map(pl => <Link to={ "/products/" + pl }>Item{<br />}</Link>)}
                 </div>
             </div>
         </div>
